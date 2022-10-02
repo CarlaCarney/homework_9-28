c25819a1c2f1f82f6714da6d3485be615f12470b
Carla Carney <carlacarney2@gmail.com>
2022-10-02 18:02:12 -0300

    homework 9-28

---
+++ question2.py
@@ -0,0 +1,12 @@
+def area(length, width):
+    square_feet = (length * width)
+    print(f'Your house is {square_feet} square feet.')
+
+print(area(40,25))
+
+
+def circle(r):
+    cir = (3.14*r*2)
+    return cir
+
+circle(6)
